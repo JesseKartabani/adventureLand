9dcbe4f99d60aa15dded77e35a24e0675b18baf7
Jesse1224 <jessekartabani@gmail.com>
2022-03-10 17:06:12 +1100

    Hunter skills bug fix

--- codes/master.5.js
+++ codes/master.5.js
@@ -601,7 +601,9 @@ function hunter_skills() {
             var target = get_targeted_monster();
             setInterval(function () {
                 // Casts 3shot every 2 seconds if can use
-                if (can_use("3shot")) use_skill("3shot", target);
+                if (can_use("3shot")) {
+                    use_skill("3shot", target);
+                }
             }, 2000);
         }
     }
