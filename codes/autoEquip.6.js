a6effd71f485be89317a9d377b30b3da391b4e93
Jesse1224 <jessekartabani@gmail.com>
2022-03-11 18:44:07 +1100

    Removed useless function

--- codes/autoEquip.6.js
+++ codes/autoEquip.6.js
@@ -3,9 +3,7 @@
 // character.bank.items1[]
 const bank_loaction = [true, { map: 'bank', x: 4, y: -370 }];
 
-function addButtons() {
-    add_bottom_button("autoEquip", "🛡", auto_equip);
-}
+add_bottom_button("autoEquip", "🛡", auto_equip);
 
 function auto_equip() {
     smart_move(bank_loaction[1]);
