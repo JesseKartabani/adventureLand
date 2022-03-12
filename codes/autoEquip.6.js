c90da3273231ec17967ad9f4f6ab0d619230d53b
Jesse1224 <jessekartabani@gmail.com>
2022-03-12 21:41:58 +1100

    Added retrieve bank item function

--- codes/autoEquip.6.js
+++ codes/autoEquip.6.js
@@ -1,12 +1,45 @@
 // How to access each bank teller
 // character.bank.items0[]
 // character.bank.items1[]
+
+const ranger_set = [
+    "wcap",
+    "wbreeches"
+]
+
 const bank_loaction = [true, { map: 'bank', x: 4, y: -370 }];
 
 // no more button instead do it on 50 min interval 
 
 function auto_equip() {
-    if (character.ctype != "merchant"){
+    // If character isn't our merchant
+    if (character.ctype != "merchant") {
+        // Move to the back of the bank
         smart_move(bank_loaction[1]);
+        // Once we are inside the bank
+        if (character.map == "bank") {
+           retrieve_bank_item(ranger_set, 7)
+        }
     }
 }
+
+// We loop through the bank to find and retieve our chosen items
+function retrieve_bank_item(names, lvl) {
+    // Loops through left bankers items
+    for (let i in character.bank.items0) {
+        let slot = character.bank.items0[i];
+        // If the slot isn't empty
+        if (slot != null) {
+            let item = slot.name; // Name of whatever item is in current slot
+            let level = slot.level; // Level of whatever item is in current slot
+            // if the item is in our list and is high enough level
+            if (names.inculdes(item) && level >= lvl) {
+                // Take that item out of the bank
+                bank_retrieve("items0", i);
+            }
+        }
+    }
+}
+setInterval(function () {
+    auto_equip();
+}, 10000);
