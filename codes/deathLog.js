dbedd8af738aedfb718129487cbf175ce8d72816
Jesse1224 <jessekartabani@gmail.com>
2022-03-21 17:54:43 +1100

    Add death log sudo code

---
+++ codes/deathLog.js
@@ -0,0 +1,9 @@
+// Log character deaths into csv (char name, killed by, time, date)
+
+// sudo code
+
+// if !character.rip return
+// get currrent date and time
+// get character name character.name
+// get name of mob that last hit us
+// write to file
